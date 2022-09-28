abcb96255ffc8fee8969c223a828dde470ad015b
MathisPastreNelson <shiray@hotmail.fr>
2022-09-28 14:58:20 +0200

    Put and Delete function OK

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.get('/', (req, res) => res.send("Hello again express !"))
 app.get('/api/pokemons', (req, res) => {
     const message = `La liste des pokémons a bien été récupérée, il y en a ${pokemons.length}.`
     res.json(success(message, pokemons))
-})
+});
 
 // Route pour un pokemon unique en fonction de son ID
 app.get("/api/pokemons/:id", (req, res) => {
@@ -28,7 +28,7 @@ app.get("/api/pokemons/:id", (req, res) => {
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
     const message = `Un ${pokemon.name} a bien été trouvé.`
     res.json(success(message, pokemon))
-})
+});
 
 // Post d'un nouveau pokémon
 app.post('/api/pokemons', (req, res) => {
@@ -37,7 +37,27 @@ app.post('/api/pokemons', (req, res) => {
     pokemons.push(pokemonCreated)
     const message = `Le pokémon ${pokemonCreated.name} a bien été crée.`
     res.json(success(message, pokemonCreated))
-})
+});
+
+// Modification d'un pokemon
+app.put("/api/pokemons/:id", (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemonUpdated = { ...req.body, id: id }
+    pokemons = pokemons.map(pokemon => {
+        return pokemon.id === id ? pokemonUpdated : pokemon
+    })
+    const message = `Le pokémon ${pokemonUpdated.name} a bien été modifié.`
+    res.json(success(message, pokemonUpdated))
+});
+
+// Suppression d'un pokemon
+app.delete('/api/pokemons/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
+    pokemons = pokemons.filter(pokemon => pokemon.id !== id)
+    const message = `Le pokémon a bien été supprimé.`
+    res.json(success(message, pokemonDeleted))
+});
 
 app.listen(port, () => console.log(`Notre application node est démarrée sur : http://localhost:${port}`))
 
